c6f41f86026b535bd7c4728dab2b02d6bbfcf3c6
Alberuto <aferalo452@g.educaand.es>
2026-01-26 08:58:48 +0100

    fix text tool
    ahora puedes cambiar de texto en el script del dialogo de personajes para no atiborrar el inspector de textmeshprougui

--- Assets/Scripts/commmon/PersonajeDialogo.cs
+++ Assets/Scripts/commmon/PersonajeDialogo.cs
@@ -1,16 +1,27 @@
+using System.IO;
+using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
-using System.IO;
 
 public class PersonajeDialogo : MonoBehaviour {
 
     [Header("UI References")]
     public Image imagenPersonaje;
+    public TextMeshProUGUI textoDialogo;
+
+    [Header("Config Personaje")]
     public string nombrePersonaje; // "Kiko", "Cigala", "Fary"
     public string estado;
 
+    [Header("Lineas de diálogo")]
+    [TextArea(2, 4)]
+    public string[] lineas;        // 1,2,3,4... en Inspector
+    int indice = 0;
+
     void Start() {
+
         CambiarEstado(); //AUTOMÁTICO al cargar escena
+        ActualizarTexto(); // Mostrar primera línea
     }
     public void CambiarEstado() {
 
@@ -25,6 +36,38 @@ public class PersonajeDialogo : MonoBehaviour {
         }
         else {
             Debug.LogError($"No encontrado: {ruta}");
+        }
+    }
+    void ActualizarTexto() {
+
+        if (lineas == null || lineas.Length == 0) return;
+
+        indice = Mathf.Clamp(indice, 0, lineas.Length - 1);
+        textoDialogo.text = lineas[indice];
+    }
+
+    // Llamar desde botón "Siguiente"
+    public void SiguienteTexto() {
+
+        if (lineas == null || lineas.Length == 0) return;
+
+        if (indice < lineas.Length - 1) {
+            indice++;
+            ActualizarTexto();
+        }
+        else {
+            // Última línea , empezar pelea (solo en panel Normal / Fury, por ejemplo)
+            EmpezarPelea();
+        }
+    }
+    // Llamar desde botón "Anterior" (opcional)
+    public void AnteriorTexto() {
+
+        if (lineas == null || lineas.Length == 0) return;
+
+        if (indice > 0) {
+            indice--;
+            ActualizarTexto();
         }
     }
     public void EmpezarPelea() {
